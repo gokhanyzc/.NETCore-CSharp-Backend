c2c786ed7e7f9fd4519cd3b80b648dd98b0e04a8
gökhan yazıcı <gokhanyzc9797@gmail.com>
2022-02-19 20:26:28 +0300

    Repository base object created.

---
+++ DataAccess/Concrete/EntityFramework/EFProductDal.cs
@@ -0,0 +1,18 @@
+﻿#region
+
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public  class EFProductDal : EfEntityRepositoryBase<Product,NorthwindContext>, IProductDal
+    { 
+    }
+}
